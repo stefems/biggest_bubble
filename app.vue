1223bc6a67fde538594ca1aa1facaa012c0792b4
Austin A. King <aust.a.king@gmail.com>
2025-01-25 05:31:03 -0800

    Added functions to change value of bubble size on server side.

--- app.vue
+++ app.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-const { options } = {
+const options = {
   method: 'POST',
   body: {
-    test: 1,
+    inflate: true,
+    currentSize: 0
   }
 }
 
 const { data } = await useFetch('/api/bubble', options)
+
+console.log('initial options: ', options)
+
+const inflateBubble = () => {
+  options.body.inflate = true;
+  $fetch('/api/bubble', options)
+  console.log('inflate options: ', options)
+  console.log('inflate data: ', data)
+}
+const popBubble = () => {
+  options.body.inflate = false;
+  $fetch('/api/bubble', options)
+  console.log('pop options: ', options)
+  console.log('inflate data: ', data)
+}
+
 
 </script>
 
 <template>
   <div>
     <pre>{{ data }}</pre>
+    <button @click="inflateBubble">Size: {{ data?.currentSize }}</button>
+  </div>
+  <div>
+    <button @click="popBubble">POP!</button>
   </div>
 </template>
